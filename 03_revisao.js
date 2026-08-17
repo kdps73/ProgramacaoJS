b8743f390b039a9db061f751bf11f44437c17fc0
Kauã Piovani <kauapiovani0511@gmail.com>
2026-08-17 14:47:38 -0300

    revisao e acumulo de variáveis

---
+++ 03_revisao.js
@@ -0,0 +1,32 @@
+let aluguel = 1200.00
+let condominio = 250.59
+let internet = 85.99
+let total = aluguel + condominio + internet
+
+console.log("O total das contas é: " + total)
+
+console.log("------------------------------------------------------------------")
+
+
+let nome = "Kauã"
+let sobrenome = "Piovani"
+let nomeCompleto = nome + " "  + sobrenome
+
+console.log("Seja bem-vindo(a) " + nomeCompleto)
+
+console.log("------------------------------------------------------------------")
+
+let salario = 2000.00
+console.log("Seu salário é: R$" + salario)
+
+let comissao = 100
+salario = salario + comissao //acumulo: adicionar um novo valor na variável
+console.log("Salário com comissão: R$" + salario)
+
+let adNoturno = 200
+salario = salario + adNoturno
+console.log("Salário com adicional noturno: R$" + salario)
+/*
+
+*/ 
+
